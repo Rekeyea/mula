8fa4740db8364c79db38b3c58bc5508691f0c8f6
Emiliano Conti <econtirodriguez@outlook.com>
2016-04-05 01:13:34 -0300

    fixed width, height, positions and margins

--- app/common/services/mock.projects.js
+++ app/common/services/mock.projects.js
@@ -10,14 +10,16 @@ System.register([], function(exports_1, context_1) {
                     id: "1",
                     description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
                     images: [], mainImage: "http://www.cssauthor.com/wp-content/uploads/2015/07/Free-New-Material-Design-Backgrounds.jpg",
-                    title: "Proyecto 1"
+                    title: "Proyecto 1",
+                    subtitle: "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
                 },
                 {
                     id: "2",
                     description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
                     images: [],
                     mainImage: "http://www.wovenmonkey.com/media/wysiwyg/Elizabeth_Gomm_original_11630.png",
-                    title: "Proyecto 2"
+                    title: "Proyecto 2",
+                    subtitle: "Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt"
                 }
             ]);
         }
--- app/src/projects/types/project.ts
+++ app/src/projects/types/project.ts
@@ -1,6 +1,7 @@
 export interface IAmAProject{
     id:string;
     title:string;
+    subtitle:string;
     description:string;
     images:Array<string>;
     mainImage:string;
