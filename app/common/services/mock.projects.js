72b7ac0dcb971164ee40ac87343c64eb24af65c9
Emiliano Conti <econtirodriguez@outlook.com>
2016-04-06 22:59:32 -0300

    image urls

--- app/common/services/mock.projects.js
+++ app/common/services/mock.projects.js
@@ -9,7 +9,8 @@ System.register([], function(exports_1, context_1) {
                 {
                     id: "1",
                     description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-                    images: [], mainImage: "http://www.cssauthor.com/wp-content/uploads/2015/07/Free-New-Material-Design-Backgrounds.jpg",
+                    images: [],
+                    mainImage: "/app/assets/project1.jpg",
                     title: "Proyecto 1",
                     subtitle: "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
                 },
@@ -17,7 +18,7 @@ System.register([], function(exports_1, context_1) {
                     id: "2",
                     description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
                     images: [],
-                    mainImage: "http://www.wovenmonkey.com/media/wysiwyg/Elizabeth_Gomm_original_11630.png",
+                    mainImage: "/app/assets/project2.png",
                     title: "Proyecto 2",
                     subtitle: "Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt"
                 }
