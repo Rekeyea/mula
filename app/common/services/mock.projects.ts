da6962498fedbba400657485afe61c4cce94ea69
Emiliano Conti <econtirodriguez@outlook.com>
2016-04-16 20:57:34 -0300

    changed styles and added projects

--- app/common/services/mock.projects.ts
+++ app/common/services/mock.projects.ts
@@ -14,5 +14,19 @@ export var PROJECTS:Array<IAmAProject>=[
         images:[],        
         mainImage:"/app/assets/project2.png",
         title:"nombre del proyecto 2"
+    },
+    {
+        id:"3",
+        description:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+        images:[],        
+        mainImage:"/app/assets/project3.jpg",
+        title:"nombre del proyecto 3"
+    },
+    {
+        id:"4",
+        description:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+        images:[],        
+        mainImage:"/app/assets/project4.png",
+        title:"nombre del proyecto 4"
     }    
 ];
